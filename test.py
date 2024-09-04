dbde5adafa6eb7da2821b6dbfa6c991508aaffdd
Atila Aloise de Almeida <atila@castelonet.com>
2024-09-04 13:54:32 -0400

    vai

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-   pattern = re.compile(
+ pattern = re.compile(
         r'(\s*-\s*repository:\s*' + re.escape(repository_name) +
         r'\s*\n(?:\s+[^\n]*\n)*?' +
-        r'\s*project:\s*' + re.escape(project_name) +
-        r'\s*(?:\n\s+[^\n]*)*)\n(?=\s*-\s*repository:|\Z)', 
+        r'\s*project:\s*' + re.escape(project_name) + r'\s*\n)', 
         re.DOTALL
     )
